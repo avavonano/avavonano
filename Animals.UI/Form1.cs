0f2f1ca8aaa20fa3d79dcedd1a4501081e8af471
symeonvas <vasilikos.symeon@gmail.com>
2018-03-24 12:11:25 +0100

    card int

--- Animals.UI/Form1.cs
+++ Animals.UI/Form1.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Threading;
 using System.Windows.Forms;
-using Animals.Engine;
-using Animals.Engine.Animals;
-using Animals.Engine.Animals.Implementations;
 using Animals.Engine.UI.Interfaces;
 using Animals.Engine.GameFlow;
 using Animals.UI.UIEngineHandshake;
@@ -17,16 +13,17 @@ namespace Animals.UI
             InitializeComponent();
         }       
         private void cmdStartGame_Click(object sender, EventArgs e)
-        {
+        { 
+            int cardsNum = 5;
             bool logingOff = false;
             Labels userLabesl = new Labels(labelLifePlayer, labelDefensePlayer, labelAttackPlayer, labelUserSpecialAbilities);
             Labels pcLabesl = new Labels(labelLifeComputer, labelDefenceComputer, labelAttackComputer, labelPCSpecialAbilities);
             IUIStream userUIStream = new WindowsFormUIStream(playerConsole, logingOff, userLabesl, pictureBoxPlayer, userLifeBar);
             IUIStream pcUIStream = new WindowsFormUIStream(opponentConsole, logingOff, pcLabesl, pictureBoxOpponent, opponentLifeBar);
             IGameUIStream gameUIStream = new WindowsFormGameUIStream(txtRound, txtBoxWinner);
-            Player usr = new Player(txtUserName.Text, userUIStream, 5);
-            Player pc = new Player("Computer", pcUIStream, 5);
-            Game newGame = new Game(usr, pc, 1,gameUIStream);
+            Player usr = new Player(txtUserName.Text, userUIStream, cardsNum);
+            Player pc = new Player("Computer", pcUIStream, cardsNum);
+            Game newGame = new Game(usr, pc, cardsNum, gameUIStream);
             newGame.Duel();
         }
     }
