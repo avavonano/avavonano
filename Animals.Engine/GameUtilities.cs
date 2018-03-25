949b8aada7338360304368294b33e8d9c1155ead
symeonvas <vasilikos.symeon@gmail.com>
2018-03-25 12:50:39 +0200

    improving book-keeping of Score
    winner function with enum

---
+++ Animals.Engine/GameUtilities.cs
@@ -0,0 +1,29 @@
+﻿using Animals.Engine.UI.Interfaces;
+using Animals.Engine.UI.Interfaces.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Engine
+{
+    public static class GameUtilities
+    {
+        public static Winner GetWinner(Score score)
+        {
+            if(score .UserScore== score.PcScore)
+            {
+                return Winner.None;
+            }
+            else if(score.UserScore > score.PcScore)
+            {
+                return Winner.User;
+            }
+            else
+            {
+                return Winner.Computer;
+            }
+        }
+    }
+}
---
+++ Animals.Engine/UI.Interfaces/Score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Engine.UI.Interfaces
+{
+    public class Score
+    {
+        public int UserScore { get; set; }
+        public int PcScore { get; set; }
+        public void UpdateScore(int userScore, int pcScore)
+        {
+            UserScore += UserScore;
+            PcScore += pcScore;
+        }
+    }
+}
