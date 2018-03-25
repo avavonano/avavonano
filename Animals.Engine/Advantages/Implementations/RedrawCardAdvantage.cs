251cb1d9dc487a0da1a342ccb98ec697dcbaa423
symeonvas <vasilikos.symeon@gmail.com>
2018-03-25 17:00:18 +0200

    fix

--- Animals.Engine/Advantages/Implementations/RedrawCardAdvantage.cs
+++ Animals.Engine/Advantages/Implementations/RedrawCardAdvantage.cs
@@ -12,7 +12,7 @@ namespace Animals.Engine.Advantages.Implementations
         {
             IAnimal animalThatIsDeletd =  GameUtilities.PromptPlayerToPickCard(player, game.UIStream);
             player.Deck.Add(Draws.GetAnimal(player.UserName, player.UIStream));
-            game.UIStream.ShowUserDeck(player);
+            game.UIStream.ShowUserDeck(player, "Check out your deck after the "+GetType().Name+" advantage. If you want to continue, exit.");
         }
     }
 }
