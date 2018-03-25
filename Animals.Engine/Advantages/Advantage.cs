4639e8e0b34078f04a05ec38a05531d6ddee4963
symeonvas <vasilikos.symeon@gmail.com>
2018-03-25 14:10:43 +0200

    added infra for Advantage (extra feature where the user can exercise it before the round starts)
    added UI to pick Advantage

---
+++ Animals.Engine/Advantages/Advantage.cs
@@ -0,0 +1,23 @@
+﻿using Animals.Engine.GameFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Engine.Advantages
+{
+    public abstract class Advantage
+    {
+        public string Description { get; private set; }
+        protected Advantage(string description)
+        {
+            Description = description;
+        }
+        public void ApplyAdvantage(Player player, Game game)
+        {
+            ApplyAdvantageInternal(player, game);
+        }
+        protected abstract void ApplyAdvantageInternal(Player player, Game game);
+    }
+}
---
+++ Animals.UI/Forms/PickAdvantageForm.cs
@@ -0,0 +1,49 @@
+﻿using Animals.Engine.GameFlow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animals.UI.Forms
+{
+    public partial class PickAdvantageForm : Form
+    {
+        Player _player;
+        public int AdvantageIndex { get; private set; }
+        public PickAdvantageForm(Player player)
+        {
+            InitializeComponent();
+            _player = player;            
+            foreach (var advantage in player.Advantages)
+            {
+                advantagesListBox.Items.Add(advantage.Description);
+            }
+        }
+
+        private void pickAdvantage_Click(object sender, EventArgs e)
+        {            
+            var selectedItems = advantagesListBox.SelectedIndices;
+            if (selectedItems.Count ==0)
+            {
+                using (WarningForm warning = new WarningForm("No advantage has been selected. Please try again."))
+                {
+
+                }
+            }
+            if (selectedItems.Count>1)
+            {
+                using (WarningForm warning = new WarningForm("No more than one advantages can be applied at the same time. Please try again."))
+                {
+
+                }
+            }
+            AdvantageIndex = advantagesListBox.SelectedIndices[0];
+            Hide();
+        }
+    }
+}
