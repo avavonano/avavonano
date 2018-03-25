a981800e560759818309dbd760e8711cb4761716
symeonvas <vasilikos.symeon@gmail.com>
2018-03-25 12:49:19 +0200

    added winner pop up window

---
+++ Animals.UI/WinnerForm.cs
@@ -0,0 +1,22 @@
+﻿using Animals.Engine.UI.Interfaces.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animals.UI
+{
+    public partial class WinnerForm : Form
+    {
+        public WinnerForm(Winner winner)
+        {
+            InitializeComponent();
+            Multimedia.ShowWinner(winner, winnerPictureBox);
+        }
+    }
+}
